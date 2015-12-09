58318341d0177c6fe2b40fd01f5b442cc7f810e2
jc_jimenez <jcjj.system@gmail.com>
2015-12-09 17:12:31 -0300

    Correción a acción git-pull

--- scripts/taller-integracion-continua.sh
+++ scripts/taller-integracion-continua.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
+cd /var/www/taller-integracion-continua
+
 if [ "$1" -eq "1" ]; then
-    cd /var/www/taller-integracion-continua
     git pull
 fi
 if [ "$1" -eq "0" ]; then
@@ -8,6 +10,5 @@ if [ "$1" -eq "0" ]; then
     docker rm taller-integracion-continua_$3
     docker pull $2
     docker run -v /var/www/taller-integracion-continua:/var/www -d -i -t --name taller-integracion-continua_$3 $2 /bin/bash
-    cd /var/www/taller-integracion-continua
-    git pull https://github.com/e-gob/taller-integracion-continua
+    git pull origin $3
 fi
